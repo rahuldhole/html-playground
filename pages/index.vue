90719436e2b2235802befce20929ac053d7dd4aa
rahuldhole <rdhole95@gmail.com>
2026-05-07 18:12:20 +0000

    feat: replace landing page with dual-pane editor and output layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,15 @@
-<script setup>
-const colorMode = useColorMode()
-</script>
-
 <template>
-    <div>
-        <p class="p-4 pb-2 flex justify-end">
-            <select v-model="colorMode.preference"
-                class="border w-24 h-8 dark:bg-gray-900 dark:text-white dark:border-gray-700">
-                <option value="system">System</option>
-                <option value="light">Light</option>
-                <option value="dark">Dark</option>
-            </select>
-        </p>
-        <div class="flex justify-center">
-            <div id="editor-container" class="bg-gradient-to-r from-fuchsia-500 to-cyan-500 dark:bg-black w-100 m-4 p-10 rounded-3xl">
-                <h1 class="text-3xl text-center text-white font-bold underline hover:text-pink-200 cursor-pointer">
-                    <NuxtLink to="/editor">Open Online HTML Editor</NuxtLink>
-                </h1>
+    <div class="flex flex-col md:flex-row h-screen">
+        <ClientOnly>
+            <div id="editor-container" class="w-full md:w-1/2 h-full p-2">
+                <EditorPanel />
             </div>
-        </div>
+            <div id="output-container" class="w-full md:w-1/2 h-full p-2">
+                <EditorOutputPanel />
+            </div>
+        </ClientOnly>
     </div>
 </template>
+
+<script setup>
+</script>
